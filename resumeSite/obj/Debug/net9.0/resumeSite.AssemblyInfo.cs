ce625295560ba9f328e65774d56408b522c73430
CoombsS <CoombsS@jbu.edu>
2025-05-07 20:34:34 -0500

    Removed artifacts from repo

--- resumeSite/obj/Debug/net9.0/resumeSite.AssemblyInfo.cs
+++ resumeSite/obj/Debug/net9.0/resumeSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("resumeSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b86109807aefbc0603dac19c8dc007d7eda4217")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a00a8d5a080064794ec3ca682eed8465cf4297b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("resumeSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("resumeSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
